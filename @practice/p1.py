7e6a9fe3997e57e1f824f3a3588414d12edb290b
Hoang Cong <hodico.iam@gmail.com>
2024-07-29 21:41:03 +0700

    playing basic langchain

---
+++ @practice/p1.py
@@ -0,0 +1,70 @@
+from dotenv import load_dotenv
+from langchain.prompts import ChatPromptTemplate
+from langchain.schema.runnable import RunnableParallel, RunnableLambda
+from langchain_community.llms.ollama import Ollama
+from langchain.schema.output_parser import StrOutputParser
+
+load_dotenv()
+
+model = Ollama(model="llama3")
+
+# Templates
+
+
+promp_template = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are senior software engineer."),
+        ("human", "List the main features of this technology {technology}."),
+    ]
+)
+
+def analyze_pros(features):
+    pros_template = ChatPromptTemplate.from_messages(
+        [
+            ("system", "You are senior software engineer."),
+            (
+                "human",
+                "Given these features: {features}, list the pros of these features.",
+            ),
+        ]
+    )
+    return pros_template.format_prompt(features=features)
+
+def analyze_cons(features):
+    cons_template = ChatPromptTemplate.from_messages(
+        [
+            ("system", "You are senior software engineer."),
+            (
+                "human",
+                "Given these features: {features}, list the cons of these features.",
+            ),
+        ]
+    )
+    return cons_template.format_prompt(features=features)
+
+def combine_pros_cons(pros, cons):
+    return f"Pros:\n{pros}\n\nCons:\n{cons}"
+
+pros_branch_chain = (
+    RunnableLambda(lambda x: analyze_pros(x)) | model | StrOutputParser()
+)
+
+cons_branch_chain = (
+    RunnableLambda(lambda x: analyze_cons(x)) | model | StrOutputParser()
+)
+
+
+# Chains
+
+chain = (
+    promp_template
+    | model 
+    | StrOutputParser()
+    | RunnableParallel(branches={"pros": pros_branch_chain, "cons": cons_branch_chain})
+    | RunnableLambda(lambda x: combine_pros_cons(x["branches"]["pros"], x["branches"]["cons"]))
+)
+
+result = chain.invoke({"technology": "Kafka"})
+
+print(result)
+
